2377a942c857c4626c684a49c7875aa730b11670
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-01-08 10:43:02 +0530

    add sign-in and sign-up pages with Clerk integration

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,36 @@ import TemplatesPage from './pages/template/TemplatePage';
 
 import ATSCheckerPage from './pages/atsChecker/ATSCheckerPage';
 import { ResumeBuilder } from './components/resume-builder';
+import SignInPage from './SignIn';
+import SignUpPage from './SignUp';
+import { SignedIn, SignedOut, RedirectToSignIn } from '@clerk/clerk-react';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+
+       
         <Route path="/template" element={<TemplatesPage />} />
         <Route path="/builder" element={<ResumeBuilder />} />
         <Route path="/ats-checker" element={<ATSCheckerPage />} />
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/sign-up" element={<SignUpPage/>} />
+        <Route
+          path="/"
+          element={
+            <>
+              <SignedIn>
+                <HomePage />
+              </SignedIn>
+              <SignedOut>
+                <RedirectToSignIn />
+              </SignedOut>
+            </>
+          }
+        />
       </Routes>
+      
     </Router>
   );
 }
---
+++ src/SignIn.tsx
@@ -0,0 +1,11 @@
+import { SignIn } from '@clerk/clerk-react';
+
+const SignInPage = () => {
+  return (
+    <div style={{ margin: '2rem auto', width: '300px' }}>
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInPage;
